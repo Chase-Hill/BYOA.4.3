45f770f0a50c891180380566483ba8c7bac4f767
Chase <chaseaohill@gmail.com>
2023-02-22 17:06:31 -0500

    Updated UserController and ViewController. Also updated main storyboard

--- BYOA Chess/BYOA Chess/Controllers/ModelController/UserController.swift
+++ BYOA Chess/BYOA Chess/Controllers/ModelController/UserController.swift
@@ -9,6 +9,48 @@ import Foundation
 
 class UserController {
     
-    
-    
+    static func fetchUser(forUser user: User, completion: @escaping (User?) -> Void) {
+        
+        // MARK: - URL Construction
+        
+        guard let baseURL = URL(string: Constants.ChessURL.baseURL) else { completion(nil) ; return }
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        urlComponents?.path.append(user.username)
+        guard let finalURL = urlComponents?.url else { completion(nil) ; return }
+        
+        print("Final Chess User URL: \(finalURL)")
+        
+        // MARK: - Fetch Data Using URL
+        
+        URLSession.shared.dataTask(with: finalURL) { data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                completion(nil) ; return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Status Code: \(response.statusCode)")
+            }
+            
+            guard let data = data else { completion(nil) ; return }
+            
+            do {
+                
+                if let topLevel = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String : Any] {
+                    
+                    let userDetails = User(dictionary: topLevel)
+                    completion(userDetails)
+                    
+                }
+                
+            } catch {
+                
+                print(error.localizedDescription)
+                completion(nil) ; return
+                
+            }
+            
+        } .resume()
+    }
 }
--- BYOA Chess/BYOA Chess/Controllers/ViewController/UserViewController.swift
+++ BYOA Chess/BYOA Chess/Controllers/ViewController/UserViewController.swift
@@ -8,9 +8,43 @@
 import UIKit
 
 class UserViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var userFollowerLabel: UILabel!
+    
+    @IBOutlet weak var userLocationLabel: UILabel!
+    
+    @IBOutlet weak var userLeagueLabel: UILabel!
 
+    @IBOutlet weak var userAvatarImage: UIImageView!
+    
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    // MARK: - Properties
+    
+    var userDetails: User? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    // MARK: - Function
+    
+    func updateViews() {
+     
+        guard let userDetails = userDetails else { return }
+        userNameLabel.text = "Name: \(userDetails.name)"
+        userFollowerLabel.text = "Followers: \(userDetails.followers)"
+        userLocationLabel.text = "Location: \(userDetails.location)"
+        userLeagueLabel.text = "Current League: \(userDetails.league)"
+        userAvatarImage.image = userDetails.avatar
     }
 }
